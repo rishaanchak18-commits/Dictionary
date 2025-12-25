5cf858655778f16a5a18a8097376618e86b52136
rishaanchak18-commits <rishaanchak18@gmail.com>
2025-12-25 21:45:42 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,6 @@
+country_code={'India':91,'USA':1,'China':86,'Japan':81}
+print("Country code of India is-")
+print(country_code.get("India","Not Found"))
+
+print("Country code of UK is-")
+print(country_code.get("UK","Not Found"))
